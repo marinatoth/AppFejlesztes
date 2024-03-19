90fc853678fda94ae7e28d5f5e85859f791d9b42
marinatoth <152974077+marinatoth@users.noreply.github.com>
2024-03-19 18:06:38 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color:'#fff'}}>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#12b0b0',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff'
+  },
+});
